0fa25edff5a80bd3511dee37dfe8ceaf14f30c95
IlyaPlotnikov <ilya-plotnikov@mail.ru>
2024-09-27 21:13:23 +0300

    add card data and template

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,25 @@
 <template>
-
+    <div class="card">
+        <p>{{name}}</p>
+        <p>Атака:{{attack}}</p>
+        <p>Защита:{{defence}}</p>
+        <p>Стоимость:{{cost}}</p>
+    </div>
 </template>
 
 <script>
 export default {
-    name: "Card"
+    name: "Card",
+    props: ["name", "attack", "defence", "cost"]
 }
 </script>
 
 <style scoped>
-
+  .card {
+      background-color: moccasin;
+      height: 20vw;
+      width: 14vw;
+      font-size: 2vw;
+      padding: 5px
+  }
 </style>
