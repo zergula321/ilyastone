eeabe294bf3e63272447a2fa61afcfadc085772e
moi-ira23 <irina.sorokina232325@gmail.com>
2024-09-27 21:13:40 +0300

    add creature data and template

--- src/components/Creature.vue
+++ src/components/Creature.vue
@@ -1,13 +1,42 @@
 <template>
-
+  <div class="creature">
+      <p class="name">{{name}}</p>
+      <div class="horizontal">
+          <div class="square" style="background-color: cadetblue">{{attack}}</div>
+          <div style="flex: 1"></div>
+          <div class="square" style="background-color: crimson">{{defence}}</div>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Creature"
+    name: "Creature",
+    props: ["name", "attack", "defence"]
 }
 </script>
 
 <style scoped>
+  .creature {
+      background-color: darkgoldenrod;
+      width: 10vw;
+      height: 14vw;
+      font-size: 2vw;
+      padding: 5px
+  }
+  .name {
+      text-align: center;
+  }
+  .horizontal {
+      margin-top: 70%;
+      display: flex;
+      /*justify-content: space-between;*/
+      align-items: stretch;
+      height: 20%;
+  }
+  .square {
+      aspect-ratio: 1 / 1;
+      text-align: center;
+  }
 
 </style>
